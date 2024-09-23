9c9dbc10a01b368ce5dc2c5f927cb6c2abad9180
Kirill Lyubarskikh <kelubarskih@fintechiq.ru>
2024-09-23 23:17:08 +0200

    Initial commit

---
+++ src/main/java/com/kirill/smart_home_server/SmartHomeServerApplication.java
@@ -0,0 +1,13 @@
+package com.kirill.smart_home_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirill/smart_home_server/SmartHomeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirill.smart_home_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
